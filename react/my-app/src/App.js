b5e42474b9c90474dae0e390efd0cf0f889f3573
R.E <egami62@outlook.jp>
2024-09-05 17:10:02 +0900

    add new directory

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (submit) => {
+    submit.preventDefault();
+    console.log("onSubmit: ", name, email);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button>Button</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +27,22 @@ function App() {
           Learn React
         </a>
       </header>
+      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
+          <label>Name: </label>
+          <input
+            type={"text"}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <label>Email: </label>
+          <input
+            type={"email"}
+            style={{ marginBottom: 20 }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <button type={"submit"}>Submit</button>
+        </form>
     </div>
   );
 }
